fbc1e10ea2a6b88c83dd1de279a39583d90066bd
Mark Veerasingam <markveer70@gmail.com>
2023-10-11 23:34:44 +0100

    added @EnableFeignClients

--- src/main/java/ie/atu/y3_lab4_cicd_part2/Y3Lab4CicdPart2Application.java
+++ src/main/java/ie/atu/y3_lab4_cicd_part2/Y3Lab4CicdPart2Application.java
@@ -2,7 +2,9 @@ package ie.atu.y3_lab4_cicd_part2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class Y3Lab4CicdPart2Application {
 
